fe769cd32ea9c5f4808e5b1e79e6105d93cf2bd7
andrewthecope <andrew@thecope.net>
2017-01-21 17:45:33 -0800

    playing with the port number some more

--- index.js
+++ index.js
@@ -78,6 +78,6 @@ io.on('connection', function(socket){
 });
  
 // Listen application request on port 3000
-http.listen(5000, function(){
+http.listen(process.env.PORT || 5000, function(){
   console.log('listening on *:5000');
 });
